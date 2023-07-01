5f925733a535e992f9fd1308cff5c546b2085dc6
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-01 16:29:31 +0330

    feat(model): add department field to Employee class

--- human_resource_app/models.py
+++ human_resource_app/models.py
@@ -5,21 +5,22 @@ from django.db import models
 # Create your models here
 
 
+class Department(models.Model):
+    department_name = models.CharField(max_length=255)
+    x_coordinate = models.FloatField()
+    y_coordinate = models.FloatField()
+
 class Employee(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone = models.CharField( max_length=50)
     position = models.CharField(max_length=255)
     joined_at = models.DateTimeField(auto_now_add=True)
+    department = models.ForeignKey(Department)
 
 class TimeTrack(models.Model):
     employee = models.ForeignKey(Employee,on_delete=models.CASCADE)
     entry_time = models.DateTimeField(auto_now_add=True)
     exit_time = models.DateTimeField(auto_now_add=True)
-
-class Department(models.Model):
-    department_name = models.CharField(max_length=255)
-    x_coordinate = models.FloatField()
-    y_coordinate = models.FloatField()
 
 class CustomUser(AbstractUser):
     pass
