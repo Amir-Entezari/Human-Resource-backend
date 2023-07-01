e23209be3e603c4dba903bee4ebd788ac4312819
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-01 16:16:43 +0330

    feat(model): define employees Model

--- human_resource_app/models.py
+++ human_resource_app/models.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
 # Create your models here
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = models.CharField( max_length=50)
+    position = models.CharField(max_length=255)
+    joined_at = models.DateTimeField(auto_now_add=True)
 class CustomUser(AbstractUser):
     pass
     # add additional fields in here
