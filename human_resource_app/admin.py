42550787392f8d494c90f9bdcdf1b7b02838e4e1
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-01 18:41:37 +0330

    feat(model): register custom user model admin

--- human_resource_app/admin.py
+++ human_resource_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
 # Register your models here.
+@admin.register(CustomUser)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", 'email', 'first_name', 'last_name'),
+            },
+        ),
+    )
