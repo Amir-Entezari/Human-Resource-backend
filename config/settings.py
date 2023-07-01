45ff4d0f24c6c7700d5ecd708cdd279d9b8c8da3
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-01 16:10:07 +0330

    feat(model): set AUTH_USER_MODEL to CustomUser in human_resource_app

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'human_resource_app'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'human_resource_app.CustomUser'
