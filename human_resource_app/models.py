f6f63b1d957c93cd9edc91f1084c463c71b94632
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-02 09:54:11 +0330

    refactor(model): delete redundant field (phone) and add personal_id field

--- human_resource_app/models.py
+++ human_resource_app/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.conf import settings
 from django.db import models
 
@@ -12,7 +13,14 @@ class Department(models.Model):
 
 class Employee(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    phone = models.CharField( max_length=50)
+    personal_id = models.IntegerField(unique=True,                                  
+                                        validators=[
+                                            RegexValidator(
+                                                regex='^[0-9]{8}$',
+                                                message='Personal ID must be an 8-digit number.',
+                                                code='invalid_personal_id'
+                                            ),]
+            )
     position = models.CharField(max_length=255)
     joined_at = models.DateTimeField(auto_now_add=True)
     department = models.ForeignKey(Department,on_delete=models.CASCADE)
