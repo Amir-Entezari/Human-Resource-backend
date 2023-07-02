3c0f74b950f88810f081ee53a777dcdeb86a27d5
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-02 10:22:58 +0330

    refactor(schema): change schema's feild based on model

--- human_resource_app/schemas.py
+++ human_resource_app/schemas.py
@@ -1,5 +1,5 @@
 from ninja import ModelSchema
-from .models import TimeTrack
+from .models import TimeTrack,Employee
 class TimeTrackIn(ModelSchema):
     class Config:
         model = TimeTrack
@@ -8,3 +8,8 @@ class TimeTrackOut(ModelSchema):
     class Config:
         model = TimeTrack
         model_fields = [ "employee", "checkout_time","checkout_type"]
+
+class EmployeeCreateInOut(ModelSchema):
+    class Config:
+        model = Employee
+        model_fields = ["user","personal_id","position","joined_at","department"]
