b4cddf97d7cf244c6af5acb6c503069cefc7b39c
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-02 09:29:15 +0330

    feat(api): set csrf to true in main NinjaAPI

--- config/api.py
+++ config/api.py
@@ -2,6 +2,6 @@ from ninja import NinjaAPI
 from human_resource_app.apis import router as human_resource_app_router
 
 
-api = NinjaAPI()
+api = NinjaAPI(csrf=True)
 
 api.add_router("/human-resource/", human_resource_app_router)
