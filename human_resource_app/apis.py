de1e31496f54474c1de11f49c9ddf3cb49e038d0
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-02 09:07:59 +0330

    feat(api): create user_checkout endpoint properly

--- human_resource_app/apis.py
+++ human_resource_app/apis.py
@@ -3,10 +3,23 @@ from django.contrib.auth import login
 from ninja import Router
 from ninja.errors import HttpError
 from .models import TimeTrack
-from .schemas import TimeTrackIn
+from .schemas import TimeTrackIn,TimeTrackOut
 
 router = Router()
 
-@router.post("/user/TimeTrack/checkout")
-def user_checkout(request: HttpRequest):
-    pass
+@router.post("/user/TimeTrack/checkout",response=TimeTrackOut)
+def user_checkout(request: HttpRequest,payload:TimeTrackIn):
+    checkout = TimeTrack.objects.all().order_by('checkout_time').last()
+    new_checkout = None
+    if checkout:
+        if checkout.checkout_type == 'E':
+            new_checkout = TimeTrack(employee_id= payload.employee, checkout_time=payload.checkout_time, checkout_type='Q')
+            new_checkout.save()
+        elif checkout.checkout_type == 'Q':
+            new_checkout = TimeTrack(employee_id= payload.employee, checkout_time=payload.checkout_time, checkout_type='E')
+            new_checkout.save()
+    else:
+        new_checkout = TimeTrack(employee_id= payload.employee, checkout_time=payload.checkout_time, checkout_type='E')
+        new_checkout.save()
+
+    return new_checkout
--- human_resource_app/schemas.py
+++ human_resource_app/schemas.py
@@ -4,3 +4,7 @@ class TimeTrackIn(ModelSchema):
     class Config:
         model = TimeTrack
         model_fields = [ "employee", "checkout_time"]
+class TimeTrackOut(ModelSchema):
+    class Config:
+        model = TimeTrack
+        model_fields = [ "employee", "checkout_time","checkout_type"]
