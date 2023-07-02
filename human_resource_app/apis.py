e9514847c438b964e69894bb481c452eac3df7b0
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-02 10:23:33 +0330

    feat(api): implement creating new employee

--- human_resource_app/apis.py
+++ human_resource_app/apis.py
@@ -2,8 +2,9 @@ from django.http import HttpResponse, HttpRequest
 from django.contrib.auth import login
 from ninja import Router
 from ninja.errors import HttpError
-from .models import TimeTrack
-from .schemas import TimeTrackIn,TimeTrackOut
+from .models import TimeTrack, Employee
+from .schemas import TimeTrackIn,TimeTrackOut,EmployeeCreateInOut
+from ninja.security import django_auth
 
 router = Router()
 
@@ -23,3 +24,13 @@ def user_checkout(request: HttpRequest,payload:TimeTrackIn):
         new_checkout.save()
 
     return new_checkout
+
+@router.post("/user/create",response=EmployeeCreateInOut,auth=django_auth)
+def create_employee(request:HttpRequest,payload:EmployeeCreateInOut):
+    new_employee = Employee.objects.filter(personal_id=payload.personal_id)
+    if not new_employee:
+        new_employee = Employee(user_id=payload.user,personal_id=payload.personal_id,position=payload.position,joined_at=payload.joined_at,department_id=payload.department)
+        new_employee.save()
+        return new_employee
+    else:
+        return HttpResponse("Employee exists")
