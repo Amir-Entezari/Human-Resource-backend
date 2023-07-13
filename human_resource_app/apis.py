67ed4eba3eecd8b28fc7badbe6ab87cae62c5c68
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-13 22:44:39 +0330

    feat(api): define user login, logout, fetch apis

--- human_resource_app/apis.py
+++ human_resource_app/apis.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse, HttpRequest
 from django.contrib.auth import login
 from ninja import Router
 from ninja.errors import HttpError
-from ninja.security import django_auth
 from .models import TimeTrack, Employee, CustomUser, WorkHour
-from .schemas import TimeTrackIn,TimeTrackOut,EmployeeCreateInOut,EmployeeRetrieve,UserRetrieve
+from .schemas import TimeTrackIn,TimeTrackOut,EmployeeCreateInOut,EmployeeRetrieve,UserOut,LoginIn
 from .utils import calculate_work_hours
+from .auth import user_auth,blacklist_token,generate_token
 from datetime import date,datetime
 router = Router()
 
@@ -33,7 +33,7 @@ def user_checkout(request: HttpRequest,payload:TimeTrackIn):
 
     return new_checkout
 
-@router.post("/employees/create",response=EmployeeCreateInOut,auth=django_auth)
+@router.post("/employees/create",response=EmployeeCreateInOut,auth=user_auth)
 def create_employee(request:HttpRequest,payload:EmployeeCreateInOut):
     new_employee = Employee.objects.filter(personal_id=payload.personal_id)
     if not new_employee:
@@ -48,7 +48,7 @@ def get_all_employees(request:HttpRequest):
     employees = Employee.objects.all()
     return employees
 
-@router.get("/employees/{personal_id}",response=EmployeeRetrieve,auth=django_auth)
+@router.get("/employees/{personal_id}",response=EmployeeRetrieve,auth=user_auth)
 def get_employee(request:HttpRequest,personal_id):
     try:
         employee = Employee.objects.get(personal_id=personal_id)
@@ -67,3 +67,27 @@ def get_employee_workhour(request:HttpRequest,personal_id,start_date:date,end_da
         return {"Error": "No record was found in this date"}
     total_hour = round(calculate_work_hours(employee_time_track) / 3600,ndigits=2)
     return {"total_hour":total_hour}
+
+
+@router.get("/user", response=UserOut, auth=user_auth)
+def fetch_user(request: HttpRequest):
+    if request.auth.is_authenticated:
+        return request.auth
+    else:
+        raise HttpError(401, "User not authenticated")
+    
+@router.post("user/login")
+def user_login(request: HttpRequest, payload:LoginIn) -> HttpResponse:
+    try:
+        user = CustomUser.objects.get(username=payload.username)
+        if user.check_password(payload.password):
+            login(request, user)
+            return HttpResponse(generate_token(user), status=200) 
+        else:
+            return HttpResponse("Wrong username or password", status=401)
+    except CustomUser.DoesNotExist:
+        return HttpResponse("Wrong username or password", status=401)
+    
+@router.post("user/logout", auth=user_auth)
+def user_logout(request):
+    blacklist_token(request.headers.get("Authorization"))
