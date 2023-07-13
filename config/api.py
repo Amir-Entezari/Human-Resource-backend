ea7c48cf1397b31ed591a00a2d3d6bda8bc37ae5
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-13 22:45:23 +0330

    fix(auth): remove csrf arg in ninjaapi

--- config/api.py
+++ config/api.py
@@ -2,6 +2,6 @@ from ninja import NinjaAPI
 from human_resource_app.apis import router as human_resource_app_router
 
 
-api = NinjaAPI(csrf=True)
+api = NinjaAPI()
 
 api.add_router("/human-resource/", human_resource_app_router)
