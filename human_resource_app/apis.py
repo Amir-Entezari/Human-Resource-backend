bed50ce63721f8839cc626d83e84513d6367fff9
Amir-Entezari <mr.amirentezari@gmail.com>
2023-07-15 11:32:47 +0330

    fix(api): handle response when there is no record in get_employee

--- human_resource_app/apis.py
+++ human_resource_app/apis.py
@@ -139,9 +139,10 @@ def get_employee(request: HttpRequest, personal_id):
                 employee__personal_id=employee.personal_id,
                 checkout_time__range=[start_date, end_date],
             )
-            if not employee_time_track:
-                raise HttpError (404, "No record was found in this date")
-            employee_work_hour = WorkHour.objects.filter(employee=employee)
+            employee_work_hour = []
+            if employee_time_track:
+                # raise HttpError (404, "No record was found in this date")
+                employee_work_hour = WorkHour.objects.filter(employee=employee)
             
             first_name = employee.user.first_name
             last_name = employee.user.first_name
